3c1c805f7afdb5227765f5f079159beb8e48ed8e
Mehmet Çelik <mehmetcelik.2799@gmail.com>
2023-10-08 16:02:34 +0300

    Product Page

--- src/Pages/Products.js
+++ src/Pages/Products.js
@@ -1,31 +1,81 @@
 import React from 'react';
-import {List, Card, Button} from 'antd';
+import {List, Card, Button, Breadcrumb, Row, Col} from 'antd';
 import {Link} from 'react-router-dom';
+import {
+    PlusOutlined,
+} from '@ant-design/icons';
+
+import {Space, Table, Tag} from 'antd';
 
 const Products = () => {
-    const products = [
-        {id: 1, name: 'Ürün 1', price: 10},
-        {id: 2, name: 'Ürün 2', price: 20},
-        {id: 3, name: 'Ürün 3', price: 30},
+    const columns = [
+        {
+            title: 'Id',
+            dataIndex: 'id',
+            key: 'id',
+            render: (text) => <a>{text}</a>,
+        },
+        {
+            title: 'Ürün Adı',
+            dataIndex: 'name',
+            key: 'name',
+            render: (text) => <a>{text}</a>,
+        },
+        {
+            title: 'Fiyat',
+            dataIndex: 'price',
+            key: 'price',
+        },
+        {
+            title: 'Stok',
+            dataIndex: 'stock',
+            key: 'stock',
+        },
+
+
+        {
+            title: 'Action',
+            key: 'action',
+            render: (_, record) => (
+                <Space size="middle">
+                    <Button type="default" onClick={()=>deleteProduct(record.id)}>
+                        Sil
+                    </Button>
+                </Space>
+            ),
+        },
     ];
+
+    const data = [
+        {id: 1, name: 'İphone 15', price: 10, stock: 10},
+        {id: 2, name: 'Laptop', price: 20, stock: 15},
+        {id: 3, name: 'PlayStation', price: 30, stock: 20},
+    ];
+
+    const deleteProduct = (id) => {
+        console.log(id);
+    }
 
     return (
         <div>
-            <h1>Ürünler</h1>
-            <Link to="/create">
-                <Button type="primary">Ürün Oluştur</Button>
-            </Link>
-            <List
-                itemLayout="vertical"
-                dataSource={products}
-                renderItem={(product) => (
-                    <List.Item>
-                        <Card title={product.name}>
-                            <p>Fiyat: {product.price} TL</p>
-                        </Card>
-                    </List.Item>
-                )}
-            />
+            <Breadcrumb>
+                <Breadcrumb.Item>Anasyafa</Breadcrumb.Item>
+                <Breadcrumb.Item>Ürün Listesi</Breadcrumb.Item>
+            </Breadcrumb>
+            <Row>
+                <Col span={12}>
+                    <h1>Ürünler</h1>
+                </Col>
+                <Col span={12}>
+                    <Link to="/create">
+                        <Button type="primary" style={{float: "right"}} icon={<PlusOutlined/>}>
+                            Ürün Oluştur
+                        </Button>
+                    </Link>
+                </Col>
+            </Row>
+
+            <Table columns={columns} dataSource={data}/>
         </div>
     );
 };
